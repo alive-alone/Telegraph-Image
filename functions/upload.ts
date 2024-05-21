e0d2f33f6d35a3cebeea3c15291a3729147fbbaf
alive <2901529822@qq.com>
2024-05-21 20:20:03 +0800

    fix: kv put

--- functions/upload.ts
+++ functions/upload.ts
@@ -23,6 +23,8 @@ const addImage = async (env, id) => {
   ) {
   } else {
     //add image to kv
-    await env.img_url.put(id, "value");
+    await env.img_url.put(id, "value", {
+      metadata: { verify: "0" },
+    });
   }
 };
