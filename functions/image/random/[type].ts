ee768ada8e427f6eb8f8f36fe18b7aa594efc77d
alive <2901529822@qq.com>
2024-05-27 21:38:56 +0800

    style: remove random image API type selection

--- functions/image/random/[type].ts
+++ functions/image/random/[type].ts
@@ -1,16 +1,15 @@
 export async function onRequest(context) {
   const { request, env, params } = context;
   let datas = [] as Array<string>;
-  if (params.type) {
-  } else {
-    const keys = await env.img_url.list().keys;
-    for (let key of keys) {
-      const record = await env.img_url.getWithMetadata(key["name"]);
-      if (record.metadata && record.metadata.verify == "1") {
-        datas.push(key["name"] as string);
-      }
+
+  const keys = await env.img_url.list().keys;
+  for (let key of keys) {
+    const record = await env.img_url.getWithMetadata(key["name"]);
+    if (record.metadata && record.metadata.verify == "1") {
+      datas.push(key["name"] as string);
     }
   }
+
   const name = getRandom(datas);
   return fetch("https://telegra.ph/file/" + name, {
     method: "GET",
