775f2ada8fdc85aa043895c6d1a572dfc6bc2ccb
alive <2901529822@qq.com>
2025-02-22 20:14:32 +0800

    feat: 获取 civitai 图片列表和图片

--- functions/image/civitai/list.ts
+++ functions/image/civitai/list.ts
@@ -2,14 +2,11 @@ export async function onRequest(context) {
   const { request } = context;
   try {
     const url = new URL(request.url);
-    const response = fetch(
-      "https://image.civitai.com" + url.pathname + url.search,
-      {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-      }
-    );
+    const response = fetch("https://civitai.com/api/v1/images" + url.search, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
     return response;
   } catch (err) {
     return new Response(
